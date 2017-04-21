2afd77163b3a3b702247d86cec568bc71fe21dd0
Timo Pudas <timo.pudas@helsinki.fi>
2017-04-21 17:55:35 +0300

    Tehtävä 2 valmis

--- viikko5/MatkakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
+++ viikko5/MatkakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
@@ -40,4 +40,16 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void kortilleLadataanRahaaJosLadattavaSummaOnPositiivinen() {
+        kassa.lataa(kortti, 10);
+        verify(kortti, times(1)).lataa(10);
+    }
+    
+    @Test
+    public void kortilleEiLadataRahaaJosLadattavaSummaOnNegatiivinen() {
+        kassa.lataa(kortti, -1);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
 }
